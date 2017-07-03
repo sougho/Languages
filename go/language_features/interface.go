6828372f84576d4bf112747bbf2f34a275db5f8a
Soumen Ghosh <sghosh1@walmartlabs.com>
2017-07-03 09:25:43 +0530

    Learning

--- go/language_features/interface.go
+++ go/language_features/interface.go
@@ -9,11 +9,63 @@ type geometry interface {
 }
 
 type rect struct {
+	width, height float64
 }
 
 type circle struct {
+	radius float64
+}
+
+func (r rect) perim () float64 {
+	return (2*r.width + 2*r.height)
+}
+
+func (r rect) area () float64 {
+	return r.width * r.height
+}
+
+func (c circle) area () float64 {
+	return math.Pi*c.radius*c.radius
+}
+
+func (c circle) perim () float64 {
+	return 2*math.Pi*c.radius
+}
+
+type triangle struct {
+	base, height float64
+}
+
+func (t triangle) area() float64 {
+	return 2
+}
+
+func (t triangle) perim() float64 {
+	return 3
+}
+
+/*
+func (t triangle) perim() (float64,float64) {
+	return 3,3
+}
+*/
+
+func measure(g geometry) {
+	fmt.Println(g.area())
+	fmt.Println(g.perim())	
+}
+
+func somefunc(interface {}) (int) {
+	return 1
 }
 
 func main()  {
-		
+
+	fmt.Println("Interfaces");
+	c := circle{radius:5}
+	r := rect{width:3, height: 4}
+	t := triangle{base:2, height:3}
+	measure(c)
+	measure(r)
+	measure(t)
 }
